156b8558d831b1914a909defa65c1ea9df5d2be3
Pau Casanellas <pcasanellasp@gmail.com>
2020-10-10 21:26:15 +0200

    Get articles from strapi

---
+++ pages/_slug.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="page-home">
+    <section class="container">
+      <h1>{{ article.title }}</h1>
+      <div class="content">
+        {{ article.content }}
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $http, params }) {
+    const { slug } = params
+    const article = await $http.$get('/api/articles/', { slug })
+    return {
+      article: article[0]
+    }
+  },
+  head () {
+    const { article } = this
+    return {
+      title: article.title
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,28 @@
 <template>
-  <div class="container" />
+  <main class="page-home">
+    <section class="container">
+      <h2>Últimos artículos 📝</h2>
+      <article v-for="article in articles" :key="article.title">
+        <nuxt-link :to="{ name: 'slug', params: { slug: article.slug } }">
+          {{ article.title }}
+        </nuxt-link>
+      </article>
+    </section>
+  </main>
 </template>
+
+<script>
+export default {
+  async asyncData ({ $http }) {
+    const articles = await $http.$get('/api/articles/')
+    return {
+      articles
+    }
+  },
+  head () {
+    return {
+      title: 'Inicio'
+    }
+  }
+}
+</script>
